5e780dbf89bd681d5803043821f7f10daa365eff
sbonnefoy <bonnefoy.simon@protonmail.com>
2019-01-30 23:40:48 +0100

    make scripts for home and vpn more alike

--- iptables/iptables-home-enp0s20u1.sh
+++ iptables/iptables-home-enp0s20u1.sh
@@ -23,11 +23,12 @@ iptables -I OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o enp0s20u1 -p udp --dport 67:68 --sport 67:68 -j ACCEPT
 
 # Allow inboud ssh
-iptables -A INPUT -i enp0s20u1-p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
+iptables -A INPUT -i enp0s20u1 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
 iptables -A OUTPUT -o enp0s20u1 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
 
 # Outbout DNS lookups
 iptables -A OUTPUT -o enp0s20u1 -p udp -m udp --dport 53 -j ACCEPT
+iptables -A INPUT -i enp0s20u1 -p udp -m udp --dport 53 -j ACCEPT
 
 # Outbound ping request
 iptables -A OUTPUT -o enp0s20u1 -p icmp -j ACCEPT
@@ -39,6 +40,14 @@ iptables -A OUTPUT -o enp0s20u1 -p udp --dport 123 --sport 123 -j ACCEPT
 iptables -A OUTPUT -o enp0s20u1 -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT 
 iptables -A OUTPUT -o enp0s20u1 -p tcp -m tcp --dport 443 -m state --state NEW -j ACCEPT 
 
+# Enables port for establishing VPN
+iptables -A OUTPUT -p tcp --dport 1194 -j ACCEPT                            # enable port for establishing VPN
+iptables -A OUTPUT -p udp --dport 1194 -j ACCEPT
+# Allow communication through tunnel
+iptables -A INPUT    -i tun+ -j ACCEPT
+iptables -A OUTPUT   -o tun+ -j ACCEPT
+iptables -A FORWARD  -i tun+ -j ACCEPT
+#iptables -A OUTPUT --out-interface tun+ -j ACCEPT
 
 # Saving the rules
 /sbin/iptables-save
---
+++ iptables/iptables-vpn-home-like.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+# Firewall rules to be set while using a VPN (kill switch)
+
+# Dropping all the current rules
+iptables -F
+#iptables -t nat -F
+#iptables -t nat -X
+
+# Set policy for connections
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+
+# Setting rules for local adapter
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Setting rules for local network 
+iptables -A INPUT  -s 192.168.1.0/24 -j ACCEPT
+iptables -A OUTPUT -s 192.168.1.0/24 -j ACCEPT
+
+# VPN Establishment  
+#iptables -I INPUT --src 46.166.142.215,89.39.107.202 -j ACCEPT  # set here you vpn ip addresses
+#iptables -I OUTPUT --dst 46.166.142.215,89.39.107.202 -j ACCEPT 
+
+# Allow VPN traffic
+iptables -A INPUT  -p udp --sport 1194 -j ACCEPT #It seems that it also works without these lines...
+iptables -A OUTPUT -p udp --dport 1194 -j ACCEPT
+
+# Allow communication through tunnel
+iptables -A INPUT    -i tun+ -j ACCEPT
+iptables -A OUTPUT   -o tun+ -j ACCEPT
+iptables -A FORWARD  -i tun+ -j ACCEPT
+
+# Outbout DNS lookups
+iptables -A OUTPUT -o enp0s20u1 -p udp -m udp --dport 53 -j ACCEPT
+iptables -A INPUT -i enp0s20u1 -p udp -m udp --dport 53 -j ACCEPT
+
+# Allow inboud ssh
+iptables -A INPUT -i enp0s20u1 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -o enp0s20u1 -p tcp -m tcp --dport 22 -m state --state NEW -j ACCEPT
+
+# Accept any related or established connection
+iptables -I INPUT  -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Outbound Network Time Protocol (NTP) 
+iptables -A OUTPUT -o enp0s20u1 -p udp --dport 123 --sport 123 -j ACCEPT
+
+# Outbound HTTP and HTTPS
+iptables -A OUTPUT -o enp0s20u1 -p tcp -m tcp --dport 80 -m state --state NEW -j ACCEPT 
+iptables -A OUTPUT -o enp0s20u1 -p tcp -m tcp --dport 443 -m state --state NEW -j ACCEPT 
+
+# Allow outbond DCHP request
+iptables -A OUTPUT -o enp0s20u1 -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+
+# Bloc connection to ISP's DNS
+#iptables -I INPUT --src 103.86.99.10,60.109.121.33,62.109.121.32,ns-c-brln-04.net.telefonica.de -j DROP  # set here your ISP's DNS addresses
+#iptables -I OUTPUT --dst 103.86.99.10,62.109.121.33,62.109.121.32,ns-c-brln-04.net.telefonica.de -j DROP  # set here your ISP's DNS addresses 
